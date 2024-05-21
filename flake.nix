2b385f7211488aca317d9bc75de5de9630860ccb
Noor Mkdad <alph4nir@riseup.net>
2024-05-20 22:29:16 -0700

    Changed things around.

--- flake.nix
+++ flake.nix
@@ -1,62 +1,29 @@
 {
-  description = "A free, open source server hosting tool for Minecraft and other multiplayers.";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    rust-overlay,
-    nixpkgs,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux"];
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [cargo2nix.overlays.default];
+          };
 
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: let
-        overlays = [(import rust-overlay)];
-        pkgs = import nixpkgs {inherit system overlays;};
-        inherit (inputs.nixpkgs) lib;
-        inherit (lib) getExe;
-      in {
-        formatter = pkgs.alejandra;
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self'.packages;
-
-          packages = with pkgs; [
-            alejandra # nix formatter
-            statix # lints and suggestions
-            deadnix # clean up unused nix code
-            gcc
-            (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
-          ];
-        };
-        packages = let
-          lockFile = ./Cargo.lock;
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            rustVersion = "1.77.0";
+            packageFun = import ./Cargo.nix;
+          };
         in rec {
-          lodestone = pkgs.callPackage ./nix/default.nix {inherit inputs lockFile;};
-          default = lodestone;
-        };
-      };
-      flake = _: {
-        nixosModules = rec {
-          lodestone = import ./nix/module.nix inputs.self;
-          default = lodestone;
-        };
-      };
-    };
+          packages = {
+            # replace hello-world with your package name
+            hello-world = rustPkgs.workspace.hello-world {};
+            default = packages.hello-world;
+          };
+        }
+      );
 }
